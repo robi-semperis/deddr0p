9061842d6e2fc4f9a3b89d4b29634c187bfa1ed5
robi-semperis <robi@semperis.com>
2026-03-20 11:50:43 -0700

    Add files via upload
    For licensing, we need the hashed SID and this script, when run in the root domain, gives us the needed data.

---
+++ Get-ADDomainSidAndHashedSid.ps1
@@ -0,0 +1,16 @@
+﻿Write-Host "FQDN: $((Get-ADDomain).DNSroot)"
+
+$rootDomainSid = ""
+$hasher = [System.Security.Cryptography.HashAlgorithm]::Create('sha256')
+$hash = $hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($rootDomainSid)) 
+$hashString = [System.BitConverter]::ToString($hash)
+Write-Host "Encrypted Sid: " $hashString.Replace('-', '')
+Write-Host ""
+
+
+$rootDomainSid = (Get-ADDomain -Server (Get-ADForest).RootDomain).DomainSID.value
+$rootDomainSid
+$hasher = [System.Security.Cryptography.HashAlgorithm]::Create('sha256')
+$hash = $hasher.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($rootDomainSid))
+$hashString = [System.BitConverter]::ToString($hash)
+$hashString.Replace('-', '')
